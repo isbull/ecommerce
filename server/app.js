90e3463fcd99c2154f08d5762f7b27da11f88859
isbull <53483330+isbull@users.noreply.github.com>
2026-03-23 22:14:13 +0100

    Model Schema

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import authRoutes from "./routes/authRoute.js";
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
@@ -9,6 +10,12 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port http://localhost:${PORT}`);
+
+  connectDB();
 });
